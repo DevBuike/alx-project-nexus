5327eee2dc139c650c468ad09ac2d85fe044e1a7
DevBuike <prim3code@gmail.com>
2025-08-10 15:18:19 -0700

    fixed error

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,4 +1,7 @@
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest,
+  res: NextApiResponse) {
   const allResults = [];
   let page = 1;
   let hasNext = true;
