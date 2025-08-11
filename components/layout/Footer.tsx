2c666d6cf37d1b55ef2182bb7ded0c20aa933616
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-08-11 00:49:43 -0700

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} E-commerce app. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} iBuy. All rights reserved.</p>
         <p>
           <Link href="#" className="text-gray-400 hover:text-white">
             Privacy Policy
