79bf872ce30d0f6483157ee3b6e4259819574fe2
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-08-11 00:59:32 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -143,7 +143,7 @@ export default function Home({ categories, detailedProducts}: { categories: Cate
 
       {/* Search and Filter Section */}
       <div className="mt-20">
-        <div className="max-w-[600px] mx-auto flex gap-2 items-center mt-8 mb-4">
+        <div className="max-w-[600px] mx-auto md:flex gap-2 items-center mt-8 mb-4">
           <h1 className="text-sm text-gray-800">Filter By Price:</h1>
           <div className="flex items-center gap-4">
             <input
