9c61b1243f1b8818effd9f53c0db096220c6489e
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-08-11 03:02:10 -0700

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4">
+    <footer className="bg-[#56b8b5] text-white py-4">
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} iBuy. All rights reserved.</p>
         <p>
